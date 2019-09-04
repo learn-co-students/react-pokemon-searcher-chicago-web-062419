742f54758d1a6473aff4a02067f7024c0166c9be
Amanda Treutler <amandathedev@gmail.com>
2019-09-04 11:44:18 -0500

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,64 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+// Import functionality from library
+import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
+  // def initialize
+  constructor() {
+    // Inherits from parent
+    super();
+    // Set initial state
+    this.state = {
+      // Boolean check
+      showingBack: false
+    };
+  }
+
+  // Handle card click
+  handleClick = () => {
+    this.setState(prevState => {
+      // ?
+      return { showingBack: !prevState.showingBack };
+    });
+  };
+
+  // Console.log to see the structure of the data
+  // In this case the HP is a deeply nested value
+
+  getHp = stats => {
+    // Filter thats for the one where stat name is hp. Take the 0th one and get its value
+    stats.filter(stat => stat.name === "hp")[0].value;
+  };
+
   render() {
+    // destructure these values ONCE from props to avoid typing `this.props.pokemon.` over and over
+    const {
+      name,
+      sprites: { front, back },
+      stats
+    } = this.props.pokemon;
     return (
-      <Card>
+      // Add click listener to component
+      <Card onClick={this.handleClick}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            {/* ? */}
+            <img src={this.state.showingBack ? back : front} alt={name} />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            {/* {this.props.pokemon.name} without destructuring */}
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.getHp(stats)} hp
             </span>
           </div>
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,17 +1,57 @@
-import React from 'react'
-import { Form } from 'semantic-ui-react'
+import React from "react";
+import { Form } from "semantic-ui-react";
 
 class PokemonForm extends React.Component {
   constructor() {
-    super()
-
+    super();
+    // Set initial state with empty strings
     this.state = {
-      name: '',
-      hp: '',
-      frontUrl: '',
-      backUrl: ''
-    }
+      name: "",
+      hp: "",
+      frontUrl: "",
+      backUrl: ""
+    };
   }
+
+  handleSubmit = () => {
+    // our backend model has deeply nested data for each pokemon, so we have to build that structure manually
+    // destructure the values ONCE from state to avoid retyping `this.state.` multiple times
+    // assign the variable 'name' to this.state.name, etc.
+    const { name, hp, frontUrl, backUrl } = this.state;
+    const pokeData = {
+      name,
+      stats: [
+        {
+          name: "hp",
+          value: hp
+        }
+      ],
+      sprites: {
+        front: frontUrl,
+        back: backUrl
+      }
+    };
+    fetch("http://localhost:3000/pokemon", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(pokeData)
+    })
+      .then(resp => resp.json())
+      // Send data to addPokemon function passed in via props from
+      .then(data => this.props.addPokemon(data))
+      .catch(err => console.log(err));
+  };
+
+  // Semantic UI
+  handleChange = (event, { name, value }) => {
+    // grabbing `name` and `value` from the semantic form data - https://react.semantic-ui.com/collections/form/#usage-capture-values
+    this.setState({
+      // any key
+      [name]: value
+    });
+  };
 
   render() {
     return (
@@ -19,16 +59,41 @@ class PokemonForm extends React.Component {
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            {/* Each input needs a handleChange */}
+            <Form.Input
+              fluid
+              onChange={this.handleChange}
+              label="Name"
+              placeholder="Name"
+              name="name"
+            />
+            <Form.Input
+              fluid
+              onChange={this.handleChange}
+              label="hp"
+              placeholder="hp"
+              name="hp"
+            />
+            <Form.Input
+              fluid
+              onChange={this.handleChange}
+              label="Front Image URL"
+              placeholder="url"
+              name="frontUrl"
+            />
+            <Form.Input
+              fluid
+              onChange={this.handleChange}
+              label="Back Image URL"
+              placeholder="url"
+              name="backUrl"
+            />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
       </div>
-    )
+    );
   }
 }
 
-export default PokemonForm
+export default PokemonForm;
